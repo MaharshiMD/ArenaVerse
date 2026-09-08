aa6fa59657e41cf472089ad3ceabccaae7c04c82
MaharshiMD <maharshidihora100@gmail.com>
2026-09-08 10:41:33 +0530

    Fix ENETUNREACH SMTP error by forcing IPv4 DNS resolution

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('dns').setDefaultResultOrder('ipv4first');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
